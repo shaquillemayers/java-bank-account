bf0f3105403ab1ac03654b4cf0aef8847767c935
Shaquille Mayers <69588483+shaquillemayers@users.noreply.github.com>
2022-08-16 07:21:34 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+## Java ATM Project
+
+This is a quick beginner project, that allows a user to 
+check their balance, deposit, withdraw and more.
---
+++ src/FirstBank.java
@@ -0,0 +1,17 @@
+// Visit compscicentral.com, the #1 resource for Comp Sci students
+
+public class FirstBank {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+        Account shaq = new Account("Shaquille Mayers", "A00001");
+
+
+        Account beyonce = new Account("Ashanti", "B00002");
+        shaq.showMenu();
+
+
+    }
+
+}
